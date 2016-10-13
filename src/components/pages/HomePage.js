1c4ab253c962cac003ad14d8294186c601f3219a
ANIA-PC\Ania <angaa.m@gmail.com>
2016-10-13 22:01:15 +0200

    Basic structure of application created.

---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Pokedex app</h1>
+      <h2>Get started</h2>
+      <ol>
+        <li>Here is gonna be a great Pokedex app soon!</li>
+      </ol>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Route, IndexRoute} from 'react-router';
+
+import App from './components/App';
+import HomePage from './components/pages/HomePage';
+import AboutPage from './components/pages/AboutPage';
+import NotFoundPage from './components/pages/NotFoundPage';
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={HomePage}/>
+    <Route path="about" component={AboutPage}/>
+    <Route path='*' component={NotFoundPage}/>
+  </Route>
+);
